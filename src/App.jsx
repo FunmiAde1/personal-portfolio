82ffed8ca94c9f389fa1a2eb36707b03fe845db8
Funmi Adewoye <funmiadewoye@Funmis-MacBook-Air.local>
2025-09-25 17:46:53 -0700

    Fixed Navbar import  casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
